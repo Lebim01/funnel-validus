b81d6931b664f418355ee6da7b11da5649ecb3b4
Victor Alvarez <victor.alvarez@mtrade.mx>
2022-04-05 09:16:27 -0500

    youtube

--- src/Embudo.js
+++ src/Embudo.js
@@ -131,7 +131,7 @@ export default function Custom() {
             <iframe
               width="560"
               height="315"
-              src="https://www.youtube.com/embed/n14atT76urc?controls=0&autoplay=1"
+              src="https://www.youtube.com/embed/n14atT76urc?controls=0&autoplay=1&muted=1"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
