1bb86e23ffbc88238b7d84752f79a10c956a447a
Victor Alvarez <victor.alvarez@mtrade.mx>
2022-04-08 13:16:33 -0500

    remove unused functions

--- pages/api/user.js
+++ pages/api/user.js
@@ -5,26 +5,6 @@ const getUser = async (url) => {
     return queryresult.length > 0 ? { ...queryresult[0] } : null
 }
 
-const getValidUrl = async (url, sec = 0) => {
-    try {
-        const user = await getUser(url + `${sec === 0 ? '' : `-${sec}`}`)
-        if (user) {
-            console.log(user)
-            return getValidUrl(url, sec + 1)
-        } else {
-            return url + `${sec === 0 ? '' : `-${sec}`}`
-        }
-    } catch {
-        return url
-    }
-}
-
-export const config = {
-    api: {
-        bodyParser: false
-    }
-};
-
 export default async function handler(req, res) {
     const { method } = req
 
